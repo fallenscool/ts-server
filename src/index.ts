affd814c670212bf74f0517c5b022f202f6f8693
SpivachevskyiV <spivachevskyiv@SpivachevskyiVs-iMac.local>
2020-02-18 17:13:08 +0200

    up to 245 lss

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import { AppRouter } from './AppRouter';
+import './controllers/loginController';
 
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['asdasd'] }));
 app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(3333, () => {
   console.log('listening on port 3333');
